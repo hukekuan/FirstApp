cc7b98e0ef87d8a14f66d745656062fff0526e89
hukekuan@163.com <husd31kews86kuan>
2017-06-01 17:21:41 +0800

    局部更新

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+
+import index from '../components/page/index'
+import content from '../components/page/content'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'Hello',
+    //   component: Hello
+    // },
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'index',
+      component: index
+    }, {
+      path: '/content',
+      name: 'content',
+      component: content
     }
   ]
 })
