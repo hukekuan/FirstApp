528d2a6abd8358a7626f3f895ed8140ddee7e207
hukekuan@163.com <husd31kews86kuan>
2017-06-03 10:38:12 +0800

    局部更新

--- src/config/api.js
+++ src/config/api.js
@@ -2,24 +2,25 @@
  * Created by Administrator on 2017/6/1.
  */
 // 配置API接口地址
-var root = 'https://cnodejs.org/api/v1'
+var root = process.env.API_ROOT
 // 引用superagent
-var request = require('superagent')
+var axios = require('axios')
 // 自定义判断元素类型JS
 function toType (obj){
   return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
 }
 // 参数过滤函数
-function filter_null(o) {
+function filterNull (o) {
   for (var key in o) {
-    if (o[key] == null) {
+    if (o[key] === null) {
       delete o[key]
     }
-    if (toType(o[key]) == 'string') {
+    if (toType(o[key]) === 'string') {
       o[key] = o[key].trim()
-      if (o[key].length == 0) {
-        delete o[key]
-      }
+    } else if (toType(o[key]) === 'object') {
+      o[key] = filterNull(o[key])
+    } else if (toType(o[key]) === 'array') {
+      o[key] = filterNull(o[key])
     }
   }
   return o
@@ -31,49 +32,51 @@ function filter_null(o) {
  需要根据接口的参数进行调整。参考说明文档地址：
  https://cnodejs.org/topic/5378720ed6e2d16149fa16bd
  */
-function _api_base(method, url, params, success, failure) {
-  var r = request(method, url).type('text/plain')
+function apiAxios (method, url, params, success, failure) {
   if (params) {
-    params = filter_null(params);
-    if (method === 'POST' || method === 'PUT') {
-      if (toType(params) == 'object') {
-        params = JSON.stringify(params);
-      }
-      r = r.send(params)
-    } else if (method == 'GET' || method === 'DELETE') {
-      r = r.query(params)
-    }
+    params = filterNull(params)
   }
-  r.end(function(err, res) {
-    if (err) {
-      alert('api error, HTTP CODE: ' + res.status);
-      return;
-    };
-    if (res.body.success == true) {
-      if (success) {
-        success(res.body);
-      }
-    } else {
-      if (failure) {
-        failure(res.body);
+  axios({
+    method: method,
+    url: url,
+    data: method === 'POST' || method === 'PUT' ? params : null,
+    params: method === 'GET' || method === 'DELETE' ? params : null,
+    baseURL: root,
+    withCredentials: false
+  })
+    .then(function (res) {
+      if (res.data.success === true) {
+        if (success) {
+          success(res.data)
+        }
       } else {
-        alert('error: ' + JSON.stringify(res.body));
+        if (failure) {
+          failure(res.data)
+        } else {
+          window.alert('error: ' + JSON.stringify(res.data))
+        }
       }
-    }
-  });
-};
+    })
+    .catch(function (err) {
+      let res = err.response
+      if (err) {
+        window.alert('api error, HTTP CODE: ' + res.status)
+        return
+      }
+    })
+}
 // 返回在vue模板中的调用接口
 export default {
-  get: function(url, params, success, failure) {
-    return _api_base('GET', root + '/' + url, params, success, failure)
+  get: function (url, params, success, failure) {
+    return apiAxios('GET', url, params, success, failure)
   },
-  post: function(url, params, success, failure) {
-    return _api_base('POST', root + '/' + url, params, success, failure)
+  post: function (url, params, success, failure) {
+    return apiAxios('POST', url, params, success, failure)
   },
-  put: function(url, params, success, failure) {
-    return _api_base('PUT', root + '/' + url, params, success, failure)
+  put: function (url, params, success, failure) {
+    return apiAxios('PUT', url, params, success, failure)
   },
-  delete: function(url, params, success, failure) {
-    return _api_base('DELETE', root + '/' + url, params, success, failure)
-  },
+  delete: function (url, params, success, failure) {
+    return apiAxios('DELETE', url, params, success, failure)
+  }
 }
