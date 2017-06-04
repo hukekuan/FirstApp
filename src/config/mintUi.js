62fd1dd3cbee333667b14381a2b275fc2963ca02
hukekuan <hukekuan@163.com>
2017-06-05 00:07:09 +0800

    局部更新

---
+++ src/config/mintUi.js
@@ -0,0 +1,7 @@
+/**
+ * Created by hukekuan on 17-6-4.
+ */
+import Vue from 'vue'
+import 'mint-ui/lib/style.css'
+import Mint from 'mint-ui'
+Vue.use(Mint)
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Mint from './config/mintUi'
 import App from './App'
 import router from './router'
-
 import api from './config/api'
 Vue.prototype.$api = api
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
