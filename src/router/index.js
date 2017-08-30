90ee4e06ca6eb9506de3bc9d90899c2b5e3aaf25
hukekuan@163.com <hukekuan@163.com>
2017-08-30 18:20:49 +0800

    样式修改

--- src/router/index.js
+++ src/router/index.js
@@ -12,12 +12,12 @@ export default new Router({
       redirect: '/login'
     },
     {
-      path: '/readme',
+      path: '/main',
       component: resolve => require(['../components/common/Home.vue'],resolve),
       children: [
         {
           path: '/',
-          component: resolve => require(['../components/page/Readme.vue'],resolve)
+          component: resolve => require(['../components/page/Main.vue'],resolve)
         },
         {
           path: '/basetable',
